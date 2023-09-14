def14bf2d422992a065f83d97b1a10168bca8634
Sukrit Saha <sukritsaha27@gmail.com>
2023-09-14 20:36:41 +0530

    middleware redirection: req.nextUrl -> req.url

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,15 +8,15 @@ export async function middleware(req: NextRequest) {
   const isSignInPage = pathname === "/sign-in";
 
   if (isSignInPage && isAuthenticated) {
-    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   if (isAuthenticated && pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   if (!isAuthenticated && !isSignInPage && pathname !== "/") {
-    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+    return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 }
 

bade29972957a772a0b09e2d8dc7e1964956a9ee
Sukrit Saha <sukritsaha27@gmail.com>
2023-09-15 16:47:36 +0530

    code formatting

--- src/components/expense/add-expense.tsx
+++ src/components/expense/add-expense.tsx
@@ -73,9 +73,11 @@ export const AddExpense: FC<AddExpenseProps> = ({ expenses, calculations }) => {
   const createDownloadUrl = (blob: Blob, fileName: string) => {
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement("a");
+
     a.href = url;
     a.download = fileName;
     a.target = "_blank";
+
     document.body.appendChild(a);
     a.click();
     window.URL.revokeObjectURL(url);

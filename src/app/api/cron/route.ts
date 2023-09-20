37379d4504c2efcda605f5f0cc8b99d778f57bb4
Sukrit Saha <sukritsaha27@gmail.com>
2023-09-20 16:52:40 +0530

    cron-job testing

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,17 @@
+import { db } from "@/db";
+import { miscellaneous } from "@/db/schema";
+
+export async function GET() {
+  try {
+    await db.insert(miscellaneous).values({
+      userId: "4130d5c5-eb0b-4bbb-abbc-3c509b6e3138",
+      amount: 69,
+      entryName: "cron job",
+      entryType: "in",
+    });
+
+    return new Response("OK");
+  } catch (error) {
+    return new Response("Something went wrong", { status: 500 });
+  }
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,23 +2,13 @@ import { miscRouter } from "@/server/routers/misc";
 import { userRouter } from "@/server/routers/user";
 import { bookRouter } from "@/server/routers/book";
 import { entryRouter } from "@/server/routers/entry";
-import { createTRPCRouter, privateProcedure } from "@/server/trpc";
+import { createTRPCRouter } from "@/server/trpc";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
   books: bookRouter,
   entries: entryRouter,
   misc: miscRouter,
-  cron: privateProcedure.mutation(async ({ ctx }) => {
-    const caller = miscRouter.createCaller(ctx);
-
-    await caller.addMiscEntry({
-      amount: 69,
-      description: "cron job",
-      entryType: "in",
-      initialBalance: 1150,
-    });
-  }),
 });
 
 export type AppRouter = typeof appRouter;

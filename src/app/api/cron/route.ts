3c5d1ece067857ef0e810d007aa5fde80a106505
Sukrit Saha <sukritsaha27@gmail.com>
2023-09-21 15:18:57 +0530

    transfer previous month savings to savings a/c -> cron job

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,13 +1,36 @@
 import { db } from "@/db";
-import { miscellaneous } from "@/db/schema";
+import { books, savings } from "@/db/schema";
+import { and, sql } from "drizzle-orm";
 
 export async function GET() {
   try {
-    await db.insert(miscellaneous).values({
-      userId: "4130d5c5-eb0b-4bbb-abbc-3c509b6e3138",
-      amount: 69,
-      entryName: "Cron job test successfull",
-      entryType: "in",
+    const previousMonthBooks = await db
+      .select()
+      .from(books)
+      .where(
+        and(
+          sql`MONTH(books.createdAt) = MONTH(NOW() - INTERVAL 1 MONTH)`,
+          sql`YEAR(books.createdAt) = YEAR(NOW() - INTERVAL 1 MONTH)`
+        )
+      );
+
+    if (previousMonthBooks.length === 0) {
+      return new Response("No books to report");
+    }
+
+    previousMonthBooks.forEach(async (book) => {
+      const totalSavings = book.monthIncome - book.totalSpendings;
+      const savingMonth = book.createdAt.toLocaleString("en-US", {
+        month: "long",
+      });
+      const savingYear = book.createdAt.getFullYear();
+
+      await db.insert(savings).values({
+        amount: totalSavings > 0 ? totalSavings : 0,
+        userId: book.userId,
+        entryName: `Monthly Savings for ${savingMonth} ${savingYear}`,
+        entryType: "in",
+      });
     });
 
     return new Response("OK");

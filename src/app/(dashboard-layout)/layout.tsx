35ce8eda092718e06362bffc1464854f9f994d4f
Sukrit Saha <sukritsaha27@gmail.com>
2023-09-21 15:54:53 +0530

    redirecting new users to /onboarding from main dashboard-layout

--- src/app/(dashboard-layout)/layout.tsx
+++ src/app/(dashboard-layout)/layout.tsx
@@ -3,12 +3,18 @@ import { redirect } from "next/navigation";
 
 import { Shell } from "@/components/shell";
 import { getAuthSession } from "@/lib/auth";
+import { serverClient } from "@/trpc/server-client";
 import { DashboardSidebar } from "@/components/dashboard-sidebar";
+
+export const dynamic = "force-dynamic";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const session = await getAuthSession();
+  const currentUser = await serverClient.user.getCurrentUser();
 
   if (!session) redirect("/sign-in");
+
+  if (!currentUser.monthlyIncome) redirect("/onboarding");
 
   return (
     <div className="flex-1 items-start lg:grid lg:grid-cols-[270px_minmax(0,1fr)] h-screen">

a6b93a3ee63390f562c23e45457ceae2cf4a18c9
Sukrit Saha <sukritsaha27@gmail.com>
2023-09-21 15:39:57 +0530

    /savings ui

--- src/app/(dashboard-layout)/savings/page.tsx
+++ src/app/(dashboard-layout)/savings/page.tsx
@@ -1,5 +1,44 @@
-const Savings = () => {
-  return <div>Savings</div>;
+import { Divider } from "@nextui-org/divider";
+
+import { cn } from "@/lib/utils";
+import { CurrencyType } from "@/config";
+import { serverClient } from "@/trpc/server-client";
+import { Card, CardContent } from "@/components/ui/card";
+import SavingsCard from "@/components/cards/savings-card";
+
+export const dynamic = "force-dynamic";
+
+const Savings = async () => {
+  const currentUser = await serverClient.user.getCurrentUser();
+  const savingsEntries = await serverClient.savings.getSavingsEntries();
+
+  return (
+    <Card className="rounded-2xl">
+      <CardContent className="flex flex-col gap-y-8 py-8">
+        <div className="flex flex-col items-center gap-y-2">
+          <span
+            className={cn("text-4xl", {
+              "text-red-500": currentUser.savingsBalance < 0,
+            })}
+          >
+            <span>{currentUser.savingsBalance < 0 ? "-" : ""}</span>
+            <span>{currentUser.currency}</span>
+            {Math.abs(currentUser.savingsBalance).toLocaleString()}
+          </span>
+          <p className="text-sm text-muted-foreground tracking-tight">
+            Savings Balance
+          </p>
+        </div>
+      </CardContent>
+      <Divider />
+      <CardContent className="h-[calc(100vh-200px)] overflow-y-auto no-scrollbar pt-8">
+        <SavingsCard
+          savingsEntries={savingsEntries}
+          currency={currentUser.currency as CurrencyType}
+        />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Savings;
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,16 @@
+import { createTRPCRouter } from "@/server/trpc";
 import { miscRouter } from "@/server/routers/misc";
 import { userRouter } from "@/server/routers/user";
 import { bookRouter } from "@/server/routers/book";
 import { entryRouter } from "@/server/routers/entry";
-import { createTRPCRouter } from "@/server/trpc";
+import { savingsRouter } from "@/server/routers/savings";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
   books: bookRouter,
   entries: entryRouter,
   misc: miscRouter,
+  savings: savingsRouter,
 });
 
 export type AppRouter = typeof appRouter;

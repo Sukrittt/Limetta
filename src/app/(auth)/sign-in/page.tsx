2ee0ca2f5416fdfd2693c0a12445571374cccce5
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-05 19:32:54 +0530

    updating /sign-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
 import Balancer from "react-wrap-balancer";
 
 import AuthForm from "@/components/forms/auth-form";
 
 const SignIn = () => {
   return (
-    <div className="grid grid-cols-2 h-full">
-      <div className="flex flex-col gap-y-8 mt-8 py-24 px-28">
+    <div className="grid grid-cols-1 xl:grid-cols-2 h-full">
+      <div className="flex flex-col gap-y-8 sm:mt-8 py-24 px-8 sm:px-28">
         <div className="space-y-4">
-          <h1 className="text-4xl font-medium">
+          <h1 className="text-4xl font-medium max-w-lg xl:max-w-none">
             Begin Your Financial Journey with Balancewise
           </h1>
           <Balancer className="text-muted-foreground text-sm">
@@ -21,30 +20,8 @@ const SignIn = () => {
           <AuthForm />
         </div>
       </div>
-      <div className="p-5 h-full flex items-center justify-center">
-        <div className="relative h-[350px] w-[600px] border rounded-2xl shadow-2xl overflow-hidden">
-          <Image
-            src="/images/dashboard-snapshot.png"
-            alt="dashbaord-snapshot"
-            className="object-contain"
-            fill
-          />
-        </div>
-      </div>
     </div>
   );
 };
 
 export default SignIn;
-
-// <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-//   <div className="flex flex-col space-y-2 text-center">
-//     <h1 className="text-2xl font-semibold tracking-tight">
-//       {`Welcome to ${siteConfig.name}`}
-//     </h1>
-//     <p className="text-sm text-muted-foreground">
-//       Choose your preferred sign in method
-//     </p>
-//   </div>
-//   <AuthForm />
-// </div>

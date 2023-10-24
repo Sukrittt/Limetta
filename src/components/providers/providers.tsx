16f7e3b5207d397eda6cc8c0fc542daa2b11a210
Sukrit Saha <sukritsaha27@gmail.com>
2023-10-24 16:17:57 +0530

    adding adsense-snippet

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { httpBatchLink } from "@trpc/react-query";
 import { NextUIProvider } from "@nextui-org/react";
 import { Provider as JotaiProvider } from "jotai";
+import { Adsense } from "@ctrl/react-adsense";
+
 import { ThemeProvider } from "@/components/themes/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -34,19 +36,22 @@ export function Providers({ children }: { children: React.ReactNode }) {
   );
 
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>
-          <JotaiProvider>
-            <NextUIProvider>{children}</NextUIProvider>
-          </JotaiProvider>
-        </QueryClientProvider>
-      </trpc.Provider>
-    </ThemeProvider>
+    <>
+      <Adsense client="ca-pub-8448763122793144" slot="7259870550" />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <QueryClientProvider client={queryClient}>
+            <JotaiProvider>
+              <NextUIProvider>{children}</NextUIProvider>
+            </JotaiProvider>
+          </QueryClientProvider>
+        </trpc.Provider>
+      </ThemeProvider>
+    </>
   );
 }

d59646b5a3ba1a3b8cbfb69cebe4c0f80e925ea4
sukrit101 <sukritsaha27@gmail.com>
2023-11-11 22:38:45 +0530

    fixing site-logo

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -13,7 +13,7 @@ export const Navbar = async () => {
   return (
     <div className="sticky top-0 z-40 w-full bg-background flex justify-between items-center py-5 px-8 lg:px-24">
       <div className="relative z-20 flex items-center">
-        <SiteLogo />
+        <SiteLogo className="h-6 md:h-8" />
       </div>
       <div className="flex gap-x-4 items-center">
         {session ? (

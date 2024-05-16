bde2eec4ad581742e9f84e0dfd09642e9e3554e9
sukrittt <sukritsaha27@gmail.com>
2024-05-16 18:02:56 +0530

    adding ts ignore command

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -19,6 +19,7 @@ const DialogPortal = ({
   className,
   ...props
 }: DialogPrimitive.DialogPortalProps) => (
+  // @ts-ignore
   <DialogPrimitive.Portal className={cn(className)} {...props} />
 );
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;

3384fc7b862fa4e6babf3ee7f54e429874a46ffe
sukrittt <sukritsaha27@gmail.com>
2026-02-17 00:28:10 +0530

    drizzle migration changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-  schema: ["src/db/index.ts"],
+  schema: ["./src/db/schema.ts"],
   out: "./drizzle",
 });
